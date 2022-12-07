d7fe63f9efc30c204c71c1146ba971e0e109bfa2
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2022-12-06 21:34:24 -0500

    Updated test script

--- 4.2.0/test.sh
+++ 4.2.0/test.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 tree .
-
-echo -e '"Spartans! Ready your breakfast and eat hearty, for tonight, we dine in hell!"' | singularity exec singularity-boxes-1.3.sif boxes -d peek -a c -s 40x11
